f25ad83185f3057225b591baa7eb371f27f590fd
Gyozmo <binantguillaume@gmail.com>
2018-10-24 16:38:26 +0200

    buttons doesnt works

--- scripts.js
+++ scripts.js
@@ -18,10 +18,3 @@ city.init(streets_number, houses_number, residents_number, letters_number)
 
 // Déclenchement du facteur
 
-const getMail = () => {
-    city.postman.get_mail_from_postoffice()
-}
-
-const startTour = () => {
-    city.postman.start_tour()
-}
